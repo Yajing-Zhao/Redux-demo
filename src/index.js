7a8841759d38c65559d26b7551b4e3513b33f95e
yajing <yajing0130@gmail.com>
2020-06-24 18:03:10 -0600

    first commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {createStore} from 'redux';
+import allReducers from "./reducers/index";
+
+const store = createStore(
+  allReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ / window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+  )
+{/* 
+  // Store -> globalized states
+
+//ACTION Increment
+const increment = () => {
+  return {
+    type: "INCREMENT"
+  }
+}
+
+const decrement = () => {
+  return {
+    type: "DECREMENT"
+  }
+}
+
+// Reducer
+const counter = (state=0, action) => {
+  switch(action.type){
+    case "INCREMENT":
+      return state+1
+    case "DECREMENT":
+      return state-1
+  }
+  
+}
+
+let store = createStore(counter);
+
+// Display it in the console
+store.subscribe(()=>console.log(store.getState()))
+
+//DISPATCH
+store.dispatch(increment());
+store.dispatch(decrement());
+store.dispatch(decrement());
+*/}
+
+
 
 ReactDOM.render(
   <React.StrictMode>
